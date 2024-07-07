9b17427fadb4ee401461ffd4fbae470363f32f32
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-07-07 20:35:14 +0700

    feat: add method to upload data profile

--- lib/resources/add_data.dart
+++ lib/resources/add_data.dart
@@ -1,8 +1,10 @@
 import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 final FirebaseStorage _storage = FirebaseStorage.instance;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
 class StoreData {
   Future<String> uploadImage(String filename, Uint8List file) async {
@@ -13,5 +15,22 @@ class StoreData {
     String downloadUrl = await snapshot.ref.getDownloadURL();
 
     return downloadUrl;
+  }
+
+  Future<String> saveData(String name, String bio, Uint8List file) async {
+    String resp = 'Some Error Occured';
+    try {
+      String imgUrl = await uploadImage('profile', file);
+
+      await _firestore.collection('profile').add({
+        'name': name,
+        'bio': bio,
+        'image': imgUrl,
+      });
+      resp = 'Success';
+    } catch (err) {
+      resp = err.toString();
+    }
+    return resp;
   }
 }

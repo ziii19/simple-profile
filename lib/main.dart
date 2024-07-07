7a2b7dcb97b29bd0a547b0cd718d424246b8ad38
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-07-07 20:19:44 +0700

    feat: add method to upload image to firebase storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_profile/firebase_options.dart';
 import 'package:simple_profile/profile_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 

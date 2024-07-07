dad42ce4a414c8ea84117ef410fe07d72bbdb353
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-07-07 09:06:58 +0700

    feat: create new project simple profile

---
+++ README.md
@@ -0,0 +1,3 @@
+# simple_profile
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/simple_profile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.simple_profile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
